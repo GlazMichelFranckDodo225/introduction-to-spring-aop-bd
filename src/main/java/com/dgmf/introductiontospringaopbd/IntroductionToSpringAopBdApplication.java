eed7db68c7315108ef3e289ad50e2e388ba2d06a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-02 13:36:18 +0100

    Project Initialization

---
+++ src/main/java/com/dgmf/introductiontospringaopbd/IntroductionToSpringAopBdApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.introductiontospringaopbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringAopBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringAopBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/introductiontospringaopbd/IntroductionToSpringAopBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.introductiontospringaopbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringAopBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
